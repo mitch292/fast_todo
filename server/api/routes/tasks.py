406271e39abfb5cb506b957b889ff903f271de00
Andrew Mitchell <andrewpmitchell7@gmail.com>
2020-12-23 20:28:29 -0500

    Trailing slashes

--- server/api/routes/tasks.py
+++ server/api/routes/tasks.py
@@ -28,19 +28,19 @@ async def create_task(task: TaskInCreate, tasks_repo=Depends(TasksRepository)):
 
 
 @task_router.get(
-    "/{id}", response_description="Get a single task.", response_model=TaskInResponse
+    "/{id}/", response_description="Get a single task.", response_model=TaskInResponse
 )
 async def get_task(id: UUID, tasks_repo=Depends(TasksRepository)) -> TaskInResponse:
     return await tasks_repo.get_task_by_id(id)
 
 
-@task_router.put("/{id}", response_model=TaskInResponse)
+@task_router.put("/{id}/", response_model=TaskInResponse)
 async def update_task(
     id: UUID, task: TaskInUpdate, tasks_repo=Depends(TasksRepository)
 ):
     return await tasks_repo.update_task(id, task)
 
 
-@task_router.delete("/{id}", response_model=TaskInDelete)
+@task_router.delete("/{id}/", response_model=TaskInDelete)
 async def delete_task(id: UUID, tasks_repo=Depends(TasksRepository)):
     return await tasks_repo.delete_task(id)
